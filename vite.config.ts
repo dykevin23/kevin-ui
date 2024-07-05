a9c3f6f55097d5db6305a56234c43887b36361e8
kevin <dyk0203@naver.com>
2024-07-05 16:28:10 +0900

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,39 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { resolve } from "path";
+import dts from "vite-plugin-dts";
+import postcss from "rollup-plugin-postcss";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    dts(),
+    postcss({
+      extract: "style.css", // CSS 파일을 추출하여 별도로 저장
+      minimize: true,
+      sourceMap: true,
+    }),
+  ],
   css: {
     postcss: {
       plugins: [require("tailwindcss"), require("autoprefixer")],
     },
   },
-  // resolve: {
-  //   alias: {
-  //     "@": path.resolve(__dirname, "src"),
-  //   },
-  // },
   build: {
     lib: {
       entry: resolve(__dirname, "src/index.ts"),
       name: "index",
       fileName: "index",
+    },
+    rollupOptions: {
+      external: ["react", "react-dom"],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+        },
+      },
     },
   },
 });
