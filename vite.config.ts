51ea33240bf4e7453ab47fe00727d9859dce5daf
kevin <dyk0203@naver.com>
2024-07-05 14:44:31 +0900

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,9 +10,16 @@ export default defineConfig({
       plugins: [require("tailwindcss"), require("autoprefixer")],
     },
   },
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "src"),
+  // resolve: {
+  //   alias: {
+  //     "@": path.resolve(__dirname, "src"),
+  //   },
+  // },
+  build: {
+    lib: {
+      entry: resolve(__dirname, "src/index.ts"),
+      name: "index",
+      fileName: "index",
     },
   },
 });
