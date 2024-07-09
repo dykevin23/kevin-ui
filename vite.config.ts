1e51190f1679f56be7c4a41adf30aa51bbdcf916
kevin <dyk0203@naver.com>
2024-07-09 13:52:47 +0900

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { resolve } from "path";
-// import dts from "vite-plugin-dts";
+import dts from "vite-plugin-dts";
 import postcss from "rollup-plugin-postcss";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    // dts(),
+    dts(),
     postcss({
-      extract: "global.css",
+      // extract: "global.css",
       minimize: true,
       sourceMap: true,
       plugins: [require("tailwindcss"), require("autoprefixer")],
