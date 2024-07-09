0193f3966e2cbf11eafaccf091e826c3a6a67fab
kevin <dyk0203@naver.com>
2024-07-09 11:35:25 +0900

    update

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { resolve } from "path";
 import dts from "vite-plugin-dts";
-import postcss from "rollup-plugin-postcss";
+// import postcss from "rollup-plugin-postcss";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -16,11 +16,11 @@ export default defineConfig({
     //   plugins: [require("tailwindcss"), require("autoprefixer")],
     // }),
   ],
-  // css: {
-  //   postcss: {
-  //     plugins: [require("tailwindcss"), require("autoprefixer")],
-  //   },
-  // },
+  css: {
+    postcss: {
+      plugins: [require("tailwindcss"), require("autoprefixer")],
+    },
+  },
   build: {
     lib: {
       entry: resolve(__dirname, "src/index.ts"),
